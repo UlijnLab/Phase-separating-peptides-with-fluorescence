d26f25558c890d7896c19407a9d7e32233c83073
UlijnLab <87025755+UlijnLab@users.noreply.github.com>
2023-02-24 10:39:05 -0500

    Add files via upload
    pRDF data for Single Chain Molecular Dynamics Simulations along with shell scripts to measure using gmx tools

---
+++ pRDF_1PeptideChain/PRDF_Prot-H-OW.sh
@@ -0,0 +1,11 @@
+
+cd HGLGY_2us/
+gmx rdf -f HGLGY.xtc -s HGLGY.tpr -n index.ndx -tu ns -b 500 -rmax 1.0 -surf mol -o PRDF_HGLGY_OW-Prot-H.xvg -ref 2 -sel 23 &
+cd ../RGLGY_2us/2microseconds-analysis/
+gmx rdf -f RGLGY.xtc -s RGLGY.tpr -n index.ndx -tu ns -b 500 -rmax 1.0 -surf mol -o PRDF_RGLGY_OW-Prot-H.xvg -ref 2 -sel 23 &
+cd ../../RGSGY_2us/2microsecond-analysis/
+gmx rdf -f RGSGY.xtc -s RGSGY.tpr -n index.ndx -tu ns -b 500 -rmax 1.0 -surf mol -o PRDF_RGSGY_OW-Prot-H.xvg -ref 2 -sel 23 &
+cd ../../RSGSY_2us/
+gmx rdf -f RSGSY.xtc -s RSGSY.tpr -n index.ndx -tu ns -b 500 -rmax 1.0 -surf mol -o PRDF_RSGSY_OW-Prot-H.xvg -ref 2 -sel 24 &
+cd ..
+wait
